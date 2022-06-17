5df6b698c83ab303f0ff37b8c03db3ca7e433be0
paulclovis <paulclovis01@gmail.com>
2022-06-17 21:24:37 +0100

    Writing a book function

--- Book.cpp
+++ Book.cpp
@@ -6,8 +6,32 @@
 
 #include <utility>
 
+int Book::MaxBookId;
+
 Book::Book() = default;
 
 Book::Book(std::string author, std::string title) : mAuthor(std::move(author)), mTitle(std::move(title)) {
-//Empty
+    //Empty
 }
+
+void Book::write(std::ofstream &outStream) const {
+    outStream.write((char*) &mBookId , sizeof mBookId);
+    outStream<<mAuthor<<endl;
+    outStream<<mAuthor<<endl;
+
+    outStream.write((char*) &mBorrowed, sizeof mBorrowed);
+    outStream.write((char*) &mCustomerId, sizeof mCustomerId);
+
+    // Nous ecrivons le nombre de reseration en premier puis puis nous le l'identite apres .
+
+    {
+        int reservationListSize = mReservationList.size();
+        outStream.write((char*) &reservationListSize , sizeof reservationListSize);
+        for (int customerId:mReservationList) {
+            outStream.write((char*) &customerId,sizeof customerId);
+        }
+    }
+}
+
+
+
