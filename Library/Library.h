be7227f76f5d4b885590993fb062e70c0edc4b5e
paulclovis <paulclovis01@gmail.com>
2022-06-18 09:06:38 +0100

    Finishing with Library.h file

--- Library/Library.h
+++ Library/Library.h
@@ -5,13 +5,40 @@
 #ifndef MANLIB_LIBRARY_H
 #define MANLIB_LIBRARY_H
 #include <iostream>
+#include <set>
+#include <map>
+#include <list>
+#include <string>
+#include "../Library/Library.h"
+#include "../Customer/Customer.h"
 
+class Customer;
+class Book;
 class Library {
 public:
     Library();
 private:
     static std::string sBinaryPath;
+    lookUpBook(const std::string& author , const std::string& title, Book* bookPtr= nullptr);
+    lookUpBook(const std::string& name , const std::string& address, Customer* customerPtr= nullptr);
 
+    void addBook();
+    void deleteBook();
+    void listBook();
+
+    void addCustomer();
+    void deleteCustomer();
+    void listCustomer();
+    void borrowBook();
+    void reservedBook();
+    void returnBook();
+
+    void load();
+    void save();
+
+public:
+    static map<int, Book>sBookMap;
+    static map<int , Customer>sCustomerMap;
 
 };
 
